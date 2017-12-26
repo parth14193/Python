7651cf15e78430a9be0dcd609dc6c1556b50f685
parth14193 <parth14193@users.noreply.github.com>
2017-12-26 18:20:15 +0530

    Update read.py

--- read.py
+++ read.py
@@ -1,7 +1,7 @@
 import boto3
 
 s3 = boto3.resource('s3')
-bucket = s3.Bucket('elbacesslog')
+bucket = s3.Bucket('')
 # Iterates through all the objects, doing the pagination for you. Each obj
 # is an ObjectSummary, so it doesn't contain the body. You'll need to call
 # get to get the whole body.
@@ -10,7 +10,7 @@ for obj in bucket.objects.all():
     body = obj.get()['Body'].read()
     print body
 
-a1 = bucket.Object('S3AccessLog2017-12-21-15-43-43-377CEED9950B0FE9')
+a1 = bucket.Object('-12-21-15--43-')
 body = a1.get()['Body'].read()
 print body
  
